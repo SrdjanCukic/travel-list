7480ee5415127e7e024e69ea9b9979c687fe15a9
SrdjanCukic <cukic.srdjan@gmail.com>
2023-11-19 15:19:13 +0100

    React exercise

---
+++ src/components/Stats.js
@@ -0,0 +1,21 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const precentage = Math.round((numPacked / numItems) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {precentage === 100
+          ? "You got everything! Ready to go ✈"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${precentage}%)`}
+      </em>
+    </footer>
+  );
+}
